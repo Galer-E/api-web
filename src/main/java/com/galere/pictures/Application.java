fa985682f59521b235ee5402b19c6006a46c38e9
ilias <iliasdavid@live.fr>
2021-04-25 16:49:10 +0200

    work on tests

--- src/main/java/com/galere/pictures/Application.java
+++ src/main/java/com/galere/pictures/Application.java
@@ -2,14 +2,20 @@ package com.galere.pictures;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.galere.pictures.config.ApplicationEndHandle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		return sessionFactory;
 	}
 
 }
